20f095d01030de4e89197ba6e9dbedb8ce382656
José Miguel Torres <62628810+josemtrres@users.noreply.github.com>
2021-10-30 09:58:17 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,65 @@
 import random, math
 import networkx as nx
 import matplotlib.pyplot as plt
+from networkx.algorithms.components.biconnected import articulation_points
 from networkx.algorithms.shortest_paths import weighted
 from networkx.algorithms.traversal import breadth_first_search
 import numpy as np
+from numpy.typing import _128Bit, _256Bit
 
+#nodos = random.randrange(15, 51)
+nodos = 5
+
+arcos =  math.ceil(nodos + (nodos/2))
 
 vertices = []
 aristas = []
 aristas_random = []
-dic1  = {}
-def Generacion_Radom(n):
+matriz = []
+
+print(type(matriz))
+
+def generar_matriz(n, m):
+  
+  matriz_dispersa = []
+
+  for i in range(1, n+1):
+    for j in range(1, i +1):
+      if i != j:
+        aristas.append([i, j])
+
+  for i in range(m):
+    a = random.choice(aristas)
+    c = random.randrange(1,16)
+    aristas.remove(a)
+    aristas_random.append(a)
+    matriz_dispersa.append([a[0], a[1], c])
+
+  return matriz_dispersa
+
+def Generacion_Radom(n, m):
   G = nx.DiGraph()
-  peso = 0
-  m =  math.ceil(n + (n/2))
   print("Generando un grafo con " , n , " vertices y " , m , " arcos dirigidos\n")
 
   for i in range(1, n+1):
     G.add_node(i)
     vertices.append(i)
 
-  for i in range(1, n+1):
-    for j in range(1, i +1):
-      if i != j:
-        c = random.randrange(1, 16)
-        dic1[i, j] = str(c)
-        #G.add_edge(i, j, weight = c)
-        aristas.append((i, j))
+  matriz = generar_matriz(n, m)
 
-  for i in range(m):
-    a = random.choice(aristas)
-    aristas.remove(a)
-    aristas_random.append(a)
+  print(matriz)  
+  for i in matriz:
 
-  for i in range(len(aristas_random)):
-    c = random.choice(1, 16)
-    G.add_edge_from(aristas_random, c)
-  
-  #G.add_edges_from(aristas_random) #ESTARÁ COMENDATO HASTA SABER SI SI SE ESTÁN AGREGANDO LOS PESOS
-
+    G.add_edge(i[0], i[1], weight=i[2])
   return G
 
-#n = random.randrange(15, 51)
 
-n = 5
-G = Generacion_Radom(n)
-nx.draw(G, with_labels=True, font_weight='bold')
+G = Generacion_Radom(nodos, arcos)
+pos = nx.spring_layout(G)
+valor = dict([((u, v,), d['weight']) for u, v, d in G.edges(data=True)])
 
-# DEBUG
-#print(G[2][1]["weight"])
-
-#for i in dic1.items():
-#  print(i)
-
-# END DEBUG
+nx.draw(G, pos=pos, with_labels=True, font_weight='bold')
+nx.draw_networkx_edge_labels(G,pos=pos, edge_labels=valor)
 plt.show()
 
 
